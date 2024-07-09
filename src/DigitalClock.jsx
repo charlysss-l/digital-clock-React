cf54ea8d1932b5f5841936aba5c03a2b39ba6b05
charlysss-l <charlizemikaelanadela@gmail.com>
2024-07-09 13:18:24 +0800

    padZero

--- src/DigitalClock.jsx
+++ src/DigitalClock.jsx
@@ -21,9 +21,12 @@ function DigitalClock(){
         const meridiem = hours >= 12 ? "pm" : "am"
 
         hours = hours % 12 || 12;
-        return `${hours}:${minutes}:${seconds} ${meridiem}`
+        return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
     }
 
+    function padZero(number){
+        return( number < 10 ? "0": "") + number;
+    }
     return(
         <div className="clock-container">
            
